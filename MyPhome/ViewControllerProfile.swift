36e9804aa465b35b3af51923cd7dc5abdb5c5706
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-25 14:30:43 -0300

    criando tela para informações de contato

--- MyPhome/ViewControllerProfile.swift
+++ MyPhome/ViewControllerProfile.swift
@@ -10,7 +10,40 @@ import UIKit
 
 class ViewControllerProfile: UIViewController {
 
+    @IBOutlet weak var vrImage: UIImageView!
+    @IBOutlet weak var vrUsername: UILabel!
+    @IBOutlet weak var vrSwitchNotificacoes: UISwitch!
+    @IBOutlet weak var vrScrollOfertas: UIScrollView!
+    
+    var images = ["Calabresa","California","Mussarela","Portuguesa","Quatro Queijos", "Cervejas","Vinhos","Sucos", "Salada de Frutas","Sorvete","Torta Doce"]
+    
+    private func fillScrollView(){
+        var contentSize = 0
+        let offset = 5
+        
+        for image in images{
+            let bitmap = UIImage(named: image)
+            let ImageView = UIImageView(image: bitmap)
+            
+            //definindo o posicionamento inicial dos elementos dentro do scrollview
+            var posInicial = CGPoint(x: contentSize + offset, y: 0)
+            var tamanho = CGSize(width: 80, height: 80)
+            ImageView.frame = CGRect(origin: posInicial, size: tamanho)
+            
+            contentSize += 80 + offset
+            
+            //redefine as dimenções do scrollView
+            vrScrollOfertas.contentSize = CGSize(width: contentSize, height: 80)
+            //adiciona a nova imagem dentro do scrollView
+            vrScrollOfertas.addSubview(ImageView)
+        }
+    }
+    
+    @IBAction func handleLogoff(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
+        fillScrollView()	
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
