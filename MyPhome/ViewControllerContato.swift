a529d8b5311119ece83b5d068ae72000def5e15b
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-25 15:34:26 -0300

    alterando tipo do mapa e inserindo pino

--- MyPhome/ViewControllerContato.swift
+++ MyPhome/ViewControllerContato.swift
@@ -7,9 +7,46 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewControllerContato: UIViewController {
 
+    @IBOutlet weak var vrMap: MKMapView!
+    
+    @IBAction func handleMapType(_ sender: UISegmentedControl) {
+        
+        switch (sender.selectedSegmentIndex) {
+        case 0:
+            vrMap.mapType = .satellite
+        case 1:
+            vrMap.mapType = .hybrid
+        case 2:
+            vrMap.mapType = .standard
+        default:
+            vrMap.mapType = .standard
+        }
+    }
+    
+    //funcção utilizada para criar um pino e posicionar a camera no mapa
+    private func createPin(){
+        //criar um pino
+        let pin = MKPointAnnotation()
+        pin.title = "MyPhome"
+        pin.coordinate = CLLocationCoordinate2D(latitude: -10.17710, longitude: -48.332699)
+        
+        let zoom = MKCoordinateSpanMake(0.02,0.02)
+        
+        let regiao = MKCoordinateRegionMake(pin.coordinate, zoom)
+        
+        vrMap.setRegion(regiao, animated: true)
+    }
+    
+    @IBAction func handleCall(_ sender: UIButton) {
+        //cria e executa uma url para disparar uma ligação
+        if let url = URL(string: "tel://\(080099999)"){
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
